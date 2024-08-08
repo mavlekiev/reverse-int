9cb0582147898d59b9b404f07698253cbb561b09
mavlekiev <mavlekiev@gmail.com>
2024-08-08 15:44:40 +0400

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+  let nStr = n.toString();
+  let numberReverse;
+
+  if (nStr.length === 2) {
+    return numberReverse = parseInt(nStr[1] + nStr[0]);
+  }
+
+  if (nStr.length === 3) {
+    return numberReverse = parseInt(nStr[2] + nStr[1] + nStr[0]);
+  }
+
+  if (nStr.length === 4 && nStr[0] === '-') {
+    return numberReverse = parseInt(nStr[3] + nStr[2] + nStr[1]);
+  }
+};
